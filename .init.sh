c80cf105931f6c5ec1908a10b014a3711b06901a
César Claude <cesarclaudepro@gmail.com>
2024-02-22 13:24:56 +0100

    feat: improved script, added plugins

--- .init.sh
+++ .init.sh
@@ -2,20 +2,26 @@
 brew install zsh || apt install zsh || apt-get install zsh || yum install zsh
 
 # Install oh-my-zsh
+export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Install zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+
+# Install fonts
+mkdir -p ~/.fonts && cp -rf .fonts/* ~/.fonts
+brew install fontconfig || apt install fontconfig || apt-get install fontconfig || yum install fontconfig
+fc-cache -f -v
 
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
-# Install fonts
-mkdir -p ~/.local/share/fonts && cp -rf .fonts/* ~/.local/share/fonts
-brew install fontconfig || apt install fontconfig || apt-get install fontconfig || yum install fontconfig
-fc-cache -f -v
-
 # Overwrite dotfiles
 cp -rf .zshrc ~/
 cp -rf .p10k.zsh ~/
+cp -rf settings.json ~/.config/Code/User/
 
 # Run zsh
 exec zsh
